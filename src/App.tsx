3f93064de528fafcd553981a6ef9a399fbb55b64
zorik19 <zorikkorolev@gmail.com>
2022-10-11 15:16:45 +0300

    Удалил лишние файлы и изменил App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from 'react-router-dom'
+import {ProductPages} from "./pages/ProductPages";
+import {AboutPage} from "./pages/AboutPages";
+import {Navigation} from "./components/Navigation";
+import {HomePage} from "./pages/HomePage";
+import {FavoritePage} from "./pages/FavaritePage";
+import {MainPortPage} from "./pages/MainPortPage";
+import {AuthPage} from "./pages/AuthPage";
+import {AirportDetailPage} from "./pages/AirportDetailPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Navigation/>
+            <Routes>
+                <Route path="/" element = { <HomePage/> }/>
+                <Route path="/favorite" element = { <FavoritePage/> }/>
+                <Route path="/product" element = { <ProductPages/> }/>
+                <Route path="/about" element = { <AboutPage/> }/>
+                <Route path="/main-port" element = { <MainPortPage/> }/>
+                <Route path="/auth" element = { <AuthPage/> }/>
+                <Route path="/airport/:id" element = { <AirportDetailPage/> }/>
+            </Routes>
+        </>
+    )
 }
 
 export default App;
+//dir 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
+
